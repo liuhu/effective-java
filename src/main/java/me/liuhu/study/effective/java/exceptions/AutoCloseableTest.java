99b239f286016bef59fc26e1396e730783aa4503
liuhu <mail.liuhu@gmail.com>
2020-07-31 16:44:17 +0800

    异常处理最佳实践 AutoCloseable

---
+++ src/main/java/me/liuhu/study/effective/java/exceptions/AutoCloseableTest.java
@@ -0,0 +1,35 @@
+package me.liuhu.study.effective.java.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/7/31
+ **/
+@Slf4j
+public class AutoCloseableTest {
+    public class TestResource implements AutoCloseable {
+        public void read() throws Exception {
+            log.info("read resource ...");
+        }
+
+        @Override
+        public void close() throws Exception {
+            log.info("close resource ... ");
+        }
+    }
+
+    public void test() {
+        try (TestResource d = new TestResource()) {
+            d.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        AutoCloseableTest test = new AutoCloseableTest();
+        test.test();
+    }
+}
